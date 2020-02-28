237a78cc1929f9b76e9e427c0c03e968960c60b9
min.m <min.m@jupviec.vn>
2020-02-28 11:12:32 +0700

    Second

---
+++ Components/Route.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+} from 'react-native';
+
+export default class Route extends React.Component{
+    render() {
+        return(
+            <View style={{backgroundColor:'pink',flex:1}}>
+
+            </View>
+        );
+    }
+
+}
